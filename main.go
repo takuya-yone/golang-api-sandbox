21963c46e1d9b5306e44fe0cafefc9f8d7e64107
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2022-09-12 02:17:23 +0900

    up

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 
 func main() {
 	helloHandler := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "hello, world!\n")
+		io.WriteString(w, "Hello, world!\n")
 	}
-	http.HandleFunc("/", helloHandler)
+
+	postArticleHandler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Posting Article...\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
