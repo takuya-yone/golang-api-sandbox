1d25bb6b1c1fd64ffb77dcc1caad1f04dea4e987
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2022-09-21 03:50:40 +0900

    2.3

--- main.go
+++ main.go
@@ -13,20 +13,20 @@ import (
 )
 
 type Comment struct {
-	CommentID int
-	ArticleID int
-	Message   string
-	CreatedAt time.Time
+	CommentID int       `json:"comment_id"`
+	ArticleID int       `json:"article_id"`
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Article struct {
-	ID          int
-	Title       string
-	Contents    string
-	UserName    string
-	NiceNum     int
-	CommentList []Comment
-	CreatedAt   time.Time
+	ID          int       `json:"article_id"`
+	Title       string    `json:"title"`
+	Contents    string    `json:"contents"`
+	UserName    string    `json:"user_name"`
+	NiceNum     int       `json:"nice"`
+	CommentList []Comment `json:"comments"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 func main() {
