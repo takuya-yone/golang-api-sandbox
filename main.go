b58f26a400db3818144f1945340591a34f9cb68f
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2022-09-21 03:47:30 +0900

    2.2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -54,7 +55,14 @@ func main() {
 		CreatedAt:   time.Now(),
 	}
 
-	fmt.Printf("%+v\n", article)
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s\n", jsonData)
+
+	// fmt.Printf("%+v\n", article)
 
 	r := mux.NewRouter()
 
