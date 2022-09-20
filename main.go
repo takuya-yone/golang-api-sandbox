ecc183b916f7140f185cbfdbbd78c899083100c5
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2022-09-21 03:40:07 +0900

    up

--- main.go
+++ main.go
@@ -1,14 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/takuya-yone/golang-api-sandbox/handlers"
 )
 
+type Comment struct {
+	CommentID int
+	ArticleID int
+	Message   string
+	CreatedAt time.Time
+}
+
+type Article struct {
+	ID          int
+	Title       string
+	Contents    string
+	UserName    string
+	NiceNum     int
+	CommentList []Comment
+	CreatedAt   time.Time
+}
+
 func main() {
+
+	comment1 := Comment{
+		CommentID: 1,
+		ArticleID: 1,
+		Message:   "test comment1",
+		CreatedAt: time.Now(),
+	}
+
+	comment2 := Comment{
+		CommentID: 2,
+		ArticleID: 1,
+		Message:   "second comment",
+		CreatedAt: time.Now(),
+	}
+
+	article := Article{
+		ID:          1,
+		Title:       "first Article",
+		Contents:    "This is the test article.",
+		UserName:    "takuya-yone",
+		NiceNum:     1,
+		CommentList: []Comment{comment1, comment2},
+		CreatedAt:   time.Now(),
+	}
+
+	fmt.Printf("%+v\n", article)
 
 	r := mux.NewRouter()
 
